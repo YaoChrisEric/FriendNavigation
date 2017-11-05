b005cafd94443a5d3be3a975d07c06d6b20a5f18
yaohuasun <johnsnowsyh@gmail.com>
2017-11-05 16:25:52 -0600

    1. ported the google maps activity 2. ported the changes in default requestActivity 3. some cleanup done, more needs to be done
    --Yao

---
+++ app/src/main/java/com/example/yaohuasun/friendnavigation/MapsActivity.java
@@ -0,0 +1,246 @@
+package com.example.yaohuasun.friendnavigation;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.example.yaohuasun.friendnavigation.Models.MeetLocationModel;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback ,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
+
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private Location lastlocation;
+    private LocationRequest locationRequest;
+    private Marker currentLocationmMarker;
+    private Marker otherPartyLocationMarker;
+    double latitude,longitude;
+
+    private ValueEventListener mMeetLocationsRefListener;
+    private DatabaseReference mMeetLocationsReference;
+    private String mChatId;
+    private String mIsCallingActivityInitiator;
+
+    private FirebaseDatabase mFirebaseDatabase;
+
+    private MeetLocationModel mCurrentFriendsLocation;
+
+
+
+    //mChatId = intent.getStringExtra("ChatId");
+    //mIsCallingActivityInitiator = intent.getStringExtra("isInitiator");
+
+
+
+    // first task, display current location on map
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        Intent intent = this.getIntent();
+
+        // it is ok to use extras here because the request activity is a transient acivity (cannot go back to it)
+        // TODO: implement back arrow to the chat activity.
+        mChatId = intent.getStringExtra("ChatId");
+        mIsCallingActivityInitiator = intent.getStringExtra("isInitiator");
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+
+        mMeetLocationsReference = mFirebaseDatabase.getReference().child("BasicChat").child(mChatId).child("MeetLocation");
+
+        //add a listener for the location for basic chat, if changed, then update map markers
+        //mMeetRequestRefListener = mMeetRequestReference.addValueEventListener
+        mMeetLocationsRefListener = mMeetLocationsReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    Log.i("position1023001", "in MapsActivity, dataSnapShot is " + dataSnapshot.toString());
+                    // we had some problem retrieving the whole MeetRequestModel, so we use another method
+                    // to get the mCurrentMeetRequest instead
+
+                    if(otherPartyLocationMarker != null)
+                    {
+                        otherPartyLocationMarker.remove();
+                    }
+
+
+                    mCurrentFriendsLocation = dataSnapshot.getValue(MeetLocationModel.class);
+                    String initiatorLatitude = mCurrentFriendsLocation.getInitiatorLatitude();
+                    String initiatorLongitude = mCurrentFriendsLocation.getInitiatorLongitude();
+                    String responderLatitude = mCurrentFriendsLocation.getResponderLatitude();
+                    String responderLongitude = mCurrentFriendsLocation.getResponderLongitude();
+                    Log.i("position1023002", "in MapsActivity, initiatorLatitude is "+ initiatorLatitude+", initiatorLongitude is "+
+                            initiatorLongitude+ ", responderLongitude is "+responderLongitude + ",responder latitude is "+responderLatitude);
+
+                    LatLng latLng;
+                    double otherPartyLatitude;
+                    double otherPartyLongitude;
+                    if (mIsCallingActivityInitiator.equals("true")){
+                        // we are initiator, only need to create a marker for responder
+                        otherPartyLatitude = Double.parseDouble(responderLatitude);
+                        otherPartyLongitude = Double.parseDouble(responderLongitude);
+                        latLng = new LatLng(otherPartyLatitude,otherPartyLongitude);
+                    }
+                    else{
+                        // we are responder, only need to create a marker for initiator
+                        otherPartyLatitude = Double.parseDouble(initiatorLatitude);
+                        otherPartyLongitude = Double.parseDouble(initiatorLongitude);
+                        latLng = new LatLng(otherPartyLatitude,otherPartyLongitude);
+                    }
+
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(latLng);
+                    markerOptions.title("other Party Location");
+                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+
+                    otherPartyLocationMarker = mMap.addMarker(markerOptions);
+                    //mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        //basic chat needs new location
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        // LatLng sydney = new LatLng(-34, 151);
+        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            bulidGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(100);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED)
+        {
+            Log.i("Yao1019","location fine permission granted");
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
+        }
+        else
+        {
+            Log.i("Yao1019","permission denied");
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        lastlocation = location;
+        if(currentLocationmMarker != null)
+        {
+            currentLocationmMarker.remove();
+
+        }
+        Log.i("Yao1015","in onLocationChanged lat = "+latitude);
+        LatLng latLng = new LatLng(location.getLatitude() , location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Current Location");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        currentLocationmMarker = mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        // TODO: fix the camera problem
+        // TODO: save marker options to global, just update its position everytime
+
+        //mMap.animateCamera(CameraUpdateFactory.zoomBy(5));
+
+        //next step, save the latlang into the realtime db, and display both friends addr there
+
+        // initiatiorLatitude, initiatorLongitude or
+        // responderLatitude, responderLongitude
+
+        if(mIsCallingActivityInitiator.equals("true")){
+            mMeetLocationsReference.child("InitiatorLatitude").setValue(Double.toString(latitude));
+            mMeetLocationsReference.child("InitiatorLongitude").setValue(Double.toString(longitude));
+        }
+        else
+        {
+            mMeetLocationsReference.child("ResponderLatitude").setValue(Double.toString(latitude));
+            mMeetLocationsReference.child("ResponderLongitude").setValue(Double.toString(longitude));
+        }
+
+        if(null != mGoogleApiClient){
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
+        }
+    }
+
+    protected synchronized void bulidGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).
+                addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+
+        mGoogleApiClient.connect();
+    }
+
+    // TODO: in onDestroy or onStop, set mReceivingMeetRequest = user.getReceivingMapRequest(); to false
+}
--- app/src/main/java/com/example/yaohuasun/friendnavigation/requestActivity.java
+++ app/src/main/java/com/example/yaohuasun/friendnavigation/requestActivity.java
@@ -1,95 +1,239 @@
 package com.example.yaohuasun.friendnavigation;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.example.yaohuasun.friendnavigation.Models.MeetRequestModel;
+import com.example.yaohuasun.friendnavigation.Models.UserModel;
+import com.example.yaohuasun.friendnavigation.Utils.FNUtil;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class requestActivity extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mBasicChatRef;
+    private DatabaseReference mMeetRequestReference;
 
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
+    private DatabaseReference mUserRef;
+
+    private ValueEventListener mMeetRequestRefListener;
+
+    private String mChatId;
+
+    private boolean mIsCallingActivityInitiator;
+
+    private String mCurrentUserEmail;
+
+    private Button mAcceptBtn;
+    private Button mHanghoutBtn;
+    // the email address for the chatting friend
+    private String mFriendEmailAddr;
+
     private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
+    private MeetRequestModel mCurrentMeetRequest;
+
+    private String basicChatFriend;
+    private String mReceivingMeetRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_request);
+        // hide the system buttons
+        mContentView = findViewById(R.id.fullscreen_content);
+        // Note that some of these constants are new as of API 16 (Jelly Bean)
+        // and API 19 (KitKat). It is safe to use them, as they are inlined
+        // at compile-time and do nothing on earlier devices.
+        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
-        mVisible = true;
+        Intent intent = this.getIntent();
+
+        //SharedPreferences chatIdPref = getSharedPreferences("ChatId",MODE_PRIVATE);
+        //basicChatFriend = friendPref.getString("friendEmailAddr","defaultValue");
+        //mChatId = chatIdPref.getString("ChatId","defaultValue");
+        //Toast.makeText(this, "chat id (debug)"+mChatId, Toast.LENGTH_SHORT).show();
+
+        //mIsCallingActivityInitiator = chatIdPref.getBoolean("isInitiator",false);
+
+        // instead of getting the chatId and isInitiator from the sharedpreferences or extras.
+        // we will look at current user email and compare with current basic chat
+        // id and email addr
+
+        //mChatId = intent.getStringExtra("ChatId");
+        //mIsCallingActivityInitiator = intent.getStringExtra("isInitiator");
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        //mBasicChatRef = mFirebaseDatabase.getReference().child("BasicChat").child(mChatId);
+        mUserRef = mFirebaseDatabase.getReference().child("Users");
+
+        mAcceptBtn = (Button)findViewById(R.id.accept_button);
+
+        mCurrentMeetRequest = new MeetRequestModel();
+        mMeetRequestRefListener = null;
+
+        final FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mCurrentUserEmail = mFirebaseAuth.getCurrentUser().getEmail().trim();
+
+        //the basic chat friend still should be put into shared preferences in chat activity
+
+        mUserRef.orderByChild("emailAddr").equalTo(mCurrentUserEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (null!= dataSnapshot.getValue()){
+                    Log.i("position1010", "in OnDataChange, dataSnapShot Value is" + dataSnapshot.getValue().toString());
+                    UserModel user = dataSnapshot.child(FNUtil.encodeEmail(mCurrentUserEmail)).getValue(UserModel.class);
+                    if (user != null) {
+                        basicChatFriend = user.getCurrentChatFriend();
+                        Log.i("position1002", "basicChatFriend is" + basicChatFriend);
+                        mChatId = FNUtil.generateIDWithTwoEmails(mCurrentUserEmail, basicChatFriend );
+                        mBasicChatRef = mFirebaseDatabase.getReference().child("BasicChat").child(mChatId);
+                        mMeetRequestReference = mBasicChatRef.child("meetRequest");
+                        // mMeetRequestMessageRef = mBasicChatDatabaseRef.child(mChatId).child("meetRequest");
+                        mReceivingMeetRequest = user.getReceivingMapRequest();
+                        if(mReceivingMeetRequest.equals("false")){
+                            // the caller doesn't need the accept bubton
+                            mAcceptBtn.setVisibility(View.INVISIBLE);
+                            mIsCallingActivityInitiator = true;
+                            // need to add a listener to the responder state, when true , open the map activity,
+                            // remove the listener below
+
+                            // attach value event listener to the meetrequest reference
+                            mMeetRequestRefListener = mMeetRequestReference.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    if(dataSnapshot.exists()) {
+                                        Log.i("position09191", "in reqActivity, dataSnapShot is " + dataSnapshot.toString());
+
+                                        mCurrentMeetRequest = dataSnapshot.getValue(MeetRequestModel.class);
+                                        String initiatorEmail = mCurrentMeetRequest.getInitiatorEmailAddr();
+                                        String initiatorState = mCurrentMeetRequest.getInitiatorState();
+                                        String responderEmail = mCurrentMeetRequest.getResponderEmailaddr();
+                                        String responderState = mCurrentMeetRequest.getResponderState();
+                                        if (responderState.equals("true")){
+                                            if (!initiatorState.equals("true")){
+                                                Log.i("DEAD11", "unExpected situation,wrong, initiatorEmail is" + initiatorEmail + "initiatorState is"+initiatorState);
+                                            }
+
+                                            Intent intent = new Intent(requestActivity.this,MapsActivity.class);
+                                            //intent.putExtra("ChatId",mChatId);
+                                            //intent.putExtra("isInitiator",mIsCallingActivityInitiator);
+
+                                            // these extra info could be found in maps activity
+                                            // and start the MapsActivity
+                                            intent.putExtra("ChatId", mChatId);
+                                            intent.putExtra("isInitiator", "true");
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            });
+                        }
+                        else
+                        {
+                            mIsCallingActivityInitiator = false;
+                            Log.i("requestActivity01"," mIsCallingActivityInitiator is "+ mIsCallingActivityInitiator);
+                            mAcceptBtn.setVisibility(View.VISIBLE);
+                            mAcceptBtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    // we will modify the responderState to be true
+                                    mMeetRequestReference.child("responderState").setValue("true");
+                                    // TODO: pass in ChatId and isinitiator to map activity
+
+                                    Intent intent = new Intent(requestActivity.this,MapsActivity.class);
+
+                                    //SharedPreferences ChatIdPref = getSharedPreferences("Chat",MODE_PRIVATE);
+                                    //basicChatFriend = friendPref.getString("friendEmailAddr","defaultValue");
+                                    //boolean isInitiator = ChatIdPref.getBoolean("isInitiator",false);
+                                    //String currentChatId = ChatIdPref.getString("ChatId","defaultValue");
+
+                                    //Toast.makeText(requestActivity.this, "chatId (debug)"+ currentChatId, Toast.LENGTH_SHORT).show();
+
+                                    // and start the MapsActivity
+                                    intent.putExtra("ChatId", mChatId);
+                                    intent.putExtra("isInitiator", "false");
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            });
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        mHanghoutBtn = (Button)findViewById(R.id.hangout_button);
+        mHanghoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMeetRequestReference.child("initiatorState").setValue("false");
+                mMeetRequestReference.child("initiatorEmailAddr").setValue("");
+                mMeetRequestReference.child("responderEmailAddr").setValue("");
+                mMeetRequestReference.child("responderState").setValue("false");
+
+                mUserRef.child(FNUtil.encodeEmail(basicChatFriend)).child("receivingMapRequest").setValue("false");
+
+                Intent intent = new Intent(view.getContext(),ChatActivity.class);
+                // TODO: make a constant for string "friendEmailAddr"
+                // this extra is not the best way to pass in friend name
+                // TODO: add a listener here to the basic
+                //intent.putExtra("friendEmailAddr",mFriendEmailAddr);
+
+                startActivity(intent);
+                finish();
+                // might instead need to firstly go back to chat activity and then go to map activity
+            }
+        });
+
+
+
+
+
+        // TODO we will need to start a timer of 10 sec and beeping and timeout if user doesn't accpet and go back to ChatActivity
+        // similar to hangout button handler
+
+
+        /*mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
 
@@ -105,9 +249,13 @@ public class requestActivity extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-    }
+        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);*/
 
+
+
+
+    }
+/*
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
@@ -150,14 +298,35 @@ public class requestActivity extends AppCompatActivity {
         // Schedule a runnable to display UI elements after a delay
         mHideHandler.removeCallbacks(mHidePart2Runnable);
         mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
+    }*/
 
     /**
-     * Schedules a call to hide() in delay milliseconds, canceling any
+     * Schedules a call to hide() in [delay] milliseconds, canceling any
      * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+
+     private void delayedHide(int delayMillis) {
+     mHideHandler.removeCallbacks(mHideRunnable);
+     mHideHandler.postDelayed(mHideRunnable, delayMillis);
+     }*/
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // TODO: do the cleanup in onPause and onDestroy in every activity
+        if (null!= mMeetRequestRefListener)
+        {
+            mMeetRequestReference.removeEventListener(mMeetRequestRefListener);
+            mMeetRequestRefListener = null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null!= mMeetRequestRefListener)
+        {
+            mMeetRequestReference.removeEventListener(mMeetRequestRefListener);
+            mMeetRequestRefListener = null;
+        }
     }
 }
